28de92f7a9c16547e6bd1bfc77a8dad17bf1be27
unknown <victorchizoba30@gmail.com>
2020-08-24 15:56:58 -0700

    Initial commit

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import Card from '../components/Card'
 
-const CardList = (props) => {
+const CardList = ({robots}) => {
     return (
-        <Card />
+        <div>
+            {
+                robots.map((item) => {
+                    return (
+                        <Card
+                            key={item.id}
+                            name={item.name}
+                            email={item.email}
+                        />
+                    );
+                })
+            }
+        </div>
     );
 }
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
-import CardList from '../components/CardList'
+import CardList from '../components/CardList';
+import { robots } from '../robots';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchField: "",
+      robots: robots
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({ searchField: event.target.value })
+  }
+
   render() {
+    const filteredRobots = this.state.robots.filter((item) => {
+      return item.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    })
+
     return (
       <div className="App">
-        <h1>RoboFriends</h1>
-        <input type="text" placeholder="search robots" />
-        <CardList />
+        <h1 className="f1 light-green">RoboFriends</h1>
+        <div className='pa2'>
+          <input
+            type="text"
+            placeholder="search robots"
+            className='pa3 ba b--green bg-lightest-blue'
+            onChange={this.onInputChange} />
+        </div>
+        <CardList robots={filteredRobots} />
       </div>
-    )
+    );
   }
 }
 
