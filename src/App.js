49d68b2ac4b27ce3729ff4462b6a6aeeae99bb34
unknown <victorchizoba30@gmail.com>
2020-08-24 12:40:23 -0700

    third commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component{
-  render () {
+class App extends Component {
+  render() {
     return (
       <div className="App">
-  
+
       </div>
     )
   }
