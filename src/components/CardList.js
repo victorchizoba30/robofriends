1bcc5cd05cb42691a4613e1522b1fe967dc74669
unknown <victorchizoba30@gmail.com>
2020-08-24 13:05:18 -0700

    mew

---
+++ src/components/CardList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CardList = (props) => {
+    return (
+        <Card />
+    );
+}
+
+export default CardList;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import CardList from '../components/CardList'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <h1>RoboFriends</h1>
+        <input type="text" placeholder="search robots" />
+        <CardList />
       </div>
     )
   }
